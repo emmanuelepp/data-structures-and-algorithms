1e723bad0c7d7d1ff675e6e2c61675353911d6a0
emmanuelepp <Emmanuelponciano@gmail.com>
2021-05-14 19:51:25 -0400

    change name of the node class to sllnode class and add sll class

--- singly-linked-list.py
+++ singly-linked-list.py
@@ -13,7 +13,7 @@
 ##### #####       ##### #####       ##### #####
 
 
-class Node:
+class SLLNode:
 
     def __init__(self, data):
         self.data = data
@@ -41,3 +41,92 @@ class Node:
         """Replace the existing value on next with the value of new_next"""
 
         self.next = new_next
+
+
+class SLL:
+
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        return "Object: head={}".format(self.head)
+
+    def is_empty(self):
+
+        #True if self.head == None else False
+
+        if self.head == None:
+
+            return True
+        else:
+            return False
+
+    def add_front(self, new_data):
+        """Add a node whose data is the new_data to the front of the linked list"""
+
+        temp_node = SLLNode(new_data)
+        temp_node.set_next(self.head)
+        self.head = temp_node
+
+    def size(self):
+        """Traverses the linked list and return the number of nodez in the linked list"""
+
+        size = 0
+        if self.head == None:
+            return 0
+        else:
+            current = self.head
+            while current is not None:
+                size += 1
+                current = current.get_next
+
+            return size
+
+    def search(self, data):
+        """Traverse the linked list and find the data if the data appears in the node return true
+
+        Time complexity: O(N)
+        """
+
+        if self.head is None:
+            return 'No nodes to search in the linked list'
+
+        current = self.head
+        while current is not None:
+            if current.get_data == data:
+
+                return True
+            else:
+                current = current.get_next()
+
+        return False
+
+    def remove(self, data):
+        """Removes the firts occurence of a node that constains the data argument
+        as its data variable. Return nothing.
+
+        Time complexity: O(N)
+        """
+
+        if self.head == None:
+            return 'Linked list is empty'
+
+        current = self.head
+        previous = None
+        found = False
+
+        while not found:
+            if current.get_data() == data:
+                found = True
+            else:
+                if current.get_next() == None:
+                    return 'A node with data value is not present'
+                else:
+                    previous = current
+                    current = current.get_next()
+
+        if previous is None:
+            self.head = current.get_next()
+
+        else:
+            previous.set_data(current.get_next())
